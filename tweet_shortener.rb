f2e134ed66f5588bc6ef3ad6ebc299fb02e8f5aa
lieby weintraub <liebahinda@gmail.com>
2019-02-10 04:42:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def dictionary
+  {
+    'hello' => 'hi',
+    'to' => 2,
+    'too' => 2,
+    'two' => 2,
+    'for' => 4,
+    'four' => 4,
+    'be' => 'b',
+    'you' => 'u',
+    'at' => '@',
+    'and' => '&'
+  }
+end 
+def word_substituter(tweet)
+  split_tweet = tweet.split
+
+   split_tweet.each_with_index do |word, index|
+    dictionary.each do |to_replace, replacement|
+      if word == to_replace
+        split_tweet[index] = replacement
+      end
+    end
+  end
+split_tweet.join(" ")
+end
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end 
